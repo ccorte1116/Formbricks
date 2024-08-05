3262ea30ba3a6dedb71b9eed2a61a4fa01e6d293
volodymyr <sophia68davisb7l@outlook.com>
2024-08-05 23:36:09 +0900

    website survey

--- src/formbricks.js
+++ src/formbricks.js
@@ -1,11 +1,8 @@
-import formbricks from '@formbricks/js'
-
+import formbricks from '@formbricks/js/website'
 if (typeof window !== 'undefined') {
   formbricks.init({
-    environmentId: 'clzgrdq8j000673lkpg1pa78u',
-    userId: 'testUser',
-    apiHost: 'https://app.formbricks.com',
-    Headers: { 'x-Api-Key': '509fcfa39ba57cfdc1365ec3bb3a6c2c' }
+    environmentId: 'clzfg8ilk0006lgsuplkxhxv4',
+    apiHost: 'https://app.formbricks.com'
   })
 }
 
